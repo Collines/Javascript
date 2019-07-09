542d9607db28c0e650c3b5bfc609b7d4549e97b2
MiDo <mohammed.mido9719@gmail.com>
2019-07-09 15:19:14 +0200

    Minor fixes
    Minor fixes in display and functionality to make it looks cool

--- Show-Hide PW/script.js
+++ Show-Hide PW/script.js
@@ -1,5 +1,6 @@
 var password = document.getElementById('main').firstElementChild,
-    checkbox = document.getElementById('main').lastElementChild;
+    checkbox = document.getElementById('main').lastElementChild,
+    label = document.getElementById('main').querySelector('#main label');
 
 function hidePlaceholder(element) {
     'use strict';
@@ -14,28 +15,17 @@ function hidePlaceholder(element) {
         this.setAttribute('placeholder', myPlaceholder);
     };
 }
-
-function showOrHidePw(element) {
-    'use strict';
-    if(element.getAttribute('type') == 'password') {
-        element.setAttribute('type','text');
-    }
-    else {
-        element.setAttribute('type','password');
-    }
-}
-
     
 hidePlaceholder(password);
 
-/*checkbox.onclick = function() {
+checkbox.onclick = function() {
     'use strict';
-    if(password.getAttribute('type') == 'password') {
+    if(password.getAttribute('type') === 'password' && label.textContent == 'Show password:') {
         password.setAttribute('type','text');
+        label.textContent = 'Hide password:'
     }
     else {
         password.setAttribute('type','password');
+        label.textContent = 'Show password:';
     }
-};*/
-
-checkbox.onclick = showOrHidePw(password);
+};
